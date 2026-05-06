b46dfa95cfc01309311f041f91b1dd49d4d2ca50
Balo24 <abdullah1506@outlook.de>
2026-05-06 23:23:13 +0200

    Initial commit

---
+++ core/src/main/java/Pillarfall/pillarfall/Pillarfall_Game.java
@@ -0,0 +1,11 @@
+package Pillarfall.pillarfall;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Pillarfall_Game extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
