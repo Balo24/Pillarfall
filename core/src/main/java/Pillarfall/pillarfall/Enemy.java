0e9c6a55f75bedbb3a3ff5dd11deac3e3aa98715
Legitamiii <vinci.3nz@gmail.com>
2026-05-08 12:38:49 +0200

    Add files via upload
    Einfache Mehthoden und Rümpfe implementiert.

--- core/src/main/java/Pillarfall/pillarfall/Enemy.java
+++ core/src/main/java/Pillarfall/pillarfall/Enemy.java
@@ -1,4 +1,45 @@
 package Pillarfall.pillarfall;
 
 public class Enemy {
+
+    //Attribute / Felder
+    private int x;
+    private int y;
+
+    private int speed;
+
+    //Konstruktor
+    public Enemy(int x, int y)
+    {
+        //Initialisierung der Attribute
+        this.x = x;
+        this.y = y;
+        this.speed = 1;
+        
+    }
+
+
+  
+        //Bewegung des Gegners
+        public void move()
+        {
+            this.x += speed;
+        }
+
+        //Update Methode, die die Bewegung und andere Logiken des Gegners aktualisiert
+        public void update()
+        {
+            move();
+           
+        }
+           
+        //Rendering der position und des Gegners
+        public void render()
+        {
+            System.out.println("Gegner bei: " + x + ", " + y);
+        }
+
 }
+
+
+
